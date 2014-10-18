7dab2f99e83404c50dbabc52f41bf6bb8a8665e2
Mikael Reiersølmoen <mikael.reiersolmoen@gmail.com>
2014-10-18 16:46:45 +0200

    Fix: 4.2
    Profile can only be edited from the form in the application

--- src/webapp/controllers/UserController.php
+++ src/webapp/controllers/UserController.php
@@ -107,23 +107,33 @@ class UserController extends Controller
         }
 
         if ($this->app->request->isPost()) {
+        
             $request = $this->app->request;
             $email = $request->post('email');
             $bio = $request->post('bio');
             $age = $request->post('age');
+            $token = $request->post('csrf_token');
+            
+            if ($token == $_SESSION['csrf_token']) {
 
-            $user->setEmail($email);
-            $user->setBio($bio);
-            $user->setAge($age);
-
-            if (! User::validateAge($user)) {
-                $this->app->flashNow('error', 'Age must be between 0 and 150.');
-            } else {
-                $user->save();
-                $this->app->flashNow('info', 'Your profile was successfully saved.');
+	            $user->setEmail($email);
+	            $user->setBio($bio);
+	            $user->setAge($age);
+	
+	            if (! User::validateAge($user)) {
+	                $this->app->flashNow('error', 'Age must be between 0 and 150.');
+	            } else {
+	                $user->save();
+	                $this->app->flashNow('info', 'Your profile was successfully saved.');
+	            }
             }
         }
-
-        $this->render('edituser.twig', ['user' => $user]);
+		
+		// Create token and pass it to the rendered template
+        $_SESSION['csrf_token'] = md5(uniqid(mt_rand(), true));
+        $this->render('edituser.twig', [
+        	'user' => $user,
+        	'csrf_token' => $_SESSION['csrf_token']
+        ]);
     }
 }
